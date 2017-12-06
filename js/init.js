8b2d7c9b678d9101fcb3353d3a97b47267625a10
Morgan Porritt <mporritt@Morgans-iMac.local>
2017-12-06 13:46:52 -0700

    added nanodegree content

--- js/init.js
+++ js/init.js
@@ -3,10 +3,10 @@
 var windowHeight = $( window ).height();
 
 $("#banner").css("min-height", windowHeight + "px" );
-$(".intro-lead-in").css("height", windowHeight/2 + "px");
-$("#skills").css("min-height", windowHeight + "px" );
-$("#portfolio").css("min-height", windowHeight + "px" );
-$("#contact").css("min-height", windowHeight + "px" );
+// $(".intro-lead-in").css("height", windowHeight/2 + "px");
+// $("#skills").css("min-height", windowHeight + "px" );
+// $("#portfolio").css("min-height", windowHeight + "px" );
+// $("#contact").css("min-height", windowHeight + "px" );
 
 
 // initilize the typed.js stuff in header
@@ -14,11 +14,25 @@ $("#contact").css("min-height", windowHeight + "px" );
 $(function(){
 
     $("#typedBanner").typed({
-        strings: ["<span class = 'blue'>function</span> <span class = 'green'>initilize_portfolio</span> ( ) { </br>" + 
-        	"<span class = 'blue'>var</span> skills <span class = 'red'>=</span> { </br><span class = 'yellow tab'>     &apos;JavaScript&apos;</span>, </br><span class = 'yellow tab'>     &apos;HTML&apos;</span>, </br><span class = 'yellow tab'>     &apos;CSS&apos;</span>, </br><span class = 'yellow tab'>     &apos;PHP&apos;</span>, </br>};"
+        strings: [
+            "<span class = 'blue'>function</span> <span class = 'green'>initilize_portfolio </span> ( ) { </br>" + 
+        	"<span class = 'blue'>var</span> skills <span class = 'red'>=</span> { </br>"+
+            "<span class = 'yellow tab'> &apos;JavaScript&apos;</span>,</br>"+
+            "<span class = 'yellow tab'> &apos;HTML&apos;</span>,</br>"+
+            "<span class = 'yellow tab'> &apos;CSS&apos;</span>,</br>"+
+            "<span class = 'yellow tab'> &apos;AngularJS&apos;</span>,</br>"+
+            "<span class = 'yellow tab'> &apos;NodeJS&apos;</span>,</br>"+
+            "<span class = 'yellow tab'> &apos;MongoDB&apos;</span>,</br>"+
+            "<span class = 'yellow tab'> &apos;SQL&apos;</span>,</br>"+
+            "<span class = 'yellow tab'> &apos;PHP&apos;</span>,</br>"+
+            "<span class = 'yellow tab'> &apos;Bootstrap&apos;</span>,</br>"+
+            "<span class = 'yellow tab'> &apos;JSON&apos;</span>,</br>"+
+            "<span class = 'yellow tab'> &apos;JQUERY&apos;</span>,</br>"+
+            "<span class = 'yellow tab'> &apos;AJAX&apos;</span>,</br>"+
+            "};"
         ],
-        typeSpeed: 10,
-        backDelay: 500,
+        typeSpeed: 0,
+        backDelay: 5,
         loop: false,
         contentType: 'html', // or text
         // defaults to false for infinite loop
